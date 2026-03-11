e153facf8df6baa01bf22d0f4f2dedfdf1a305d7
dom.dom <dyasuham@gmail.com>
2026-03-11 17:10:12 +0800

    Add comments feature to film views and controller; display comment count in film list

--- app/controllers/ghiblis_controller.rb
+++ app/controllers/ghiblis_controller.rb
@@ -1,12 +1,17 @@
 class GhiblisController < ApplicationController
   def index
     @data = GhibliapiClient.new.film_list
+    film_ids = @data.map { |film| film["id"] }
+    @comments_count = Comment.where(ghibli_movie_id: film_ids).group(:ghibli_movie_id).count
   end
 
   def show
     @film = GhibliapiClient.new.get_film(params[:id])
     @film = JSON.parse(@film.body) if @film
     render_not_found unless @film
+
+    @comments = Comment.where(ghibli_movie_id: params[:id]).order(created_at: :desc).includes(:user)
+    @comment = user_signed_in? ? current_user.comments.new(ghibli_movie_id: params[:id]) : Comment.new
   end
 
   private
